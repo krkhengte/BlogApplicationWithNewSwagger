57f0a4963959a10c657707985b5d5ea6eab7fba0
Kartik K <krkhengate123@gmail.com>
2024-02-03 11:46:11 +0530

    Version Updated

--- src/main/java/jfs/backend/NewBloggerAppWithSwagger3Application.java
+++ src/main/java/jfs/backend/NewBloggerAppWithSwagger3Application.java
@@ -1,18 +1,13 @@
 package jfs.backend;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import jfs.backend.config.AppConstant;
-import jfs.backend.entity.Role;
 import jfs.backend.repository.RoleRepo;
 
 @SpringBootApplication
-public class NewBloggerAppWithSwagger3Application implements CommandLineRunner{
+public class NewBloggerAppWithSwagger3Application /* implements CommandLineRunner */{
 
 	@Autowired
 	private RoleRepo roleRepo;
@@ -21,34 +16,34 @@ public class NewBloggerAppWithSwagger3Application implements CommandLineRunner{
 		SpringApplication.run(NewBloggerAppWithSwagger3Application.class, args);
 
 	}
-	
-	@Override
-	public void run(String... args) throws Exception {
-
-		try {
-
-			Role role = new Role();
-			role.setId(AppConstant.ROLE_ADMIN);
-			role.setName("ROLE_ADMIN");
-
-			Role role1 = new Role();
-			role1.setId(AppConstant.ROLE_USER);
-			role1.setName("ROLE_USER");
-
-			List<Role> roles = List.of(role, role1);
-
-			List<Role> result = this.roleRepo.saveAll(roles);
-
-			result.forEach(r->{
-				System.out.println(r.getName());
-			});
-			
-		} catch (Exception e) {
-
-			e.printStackTrace();
-			
-		}
-
-	}
+//	
+//	@Override
+//	public void run(String... args) throws Exception {
+//
+//		try {
+//
+//			Role role = new Role();
+//			role.setId(AppConstant.ROLE_ADMIN);
+//			role.setName("ROLE_ADMIN");
+//
+//			Role role1 = new Role();
+//			role1.setId(AppConstant.ROLE_USER);
+//			role1.setName("ROLE_USER");
+//
+//			List<Role> roles = List.of(role, role1);
+//
+//			List<Role> result = this.roleRepo.saveAll(roles);
+//
+//			result.forEach(r->{
+//				System.out.println(r.getName());
+//			});
+//			
+//		} catch (Exception e) {
+//
+//			e.printStackTrace();
+//			
+//		}
+//
+//	}
 
 }
